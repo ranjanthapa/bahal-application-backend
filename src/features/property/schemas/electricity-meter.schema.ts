bdc65c6e7667733d93c2e2982a1d3938246f6a2e
ranjanthapa <ranjanthapa141@gmail.com>
2025-12-20 09:13:37 +0545

    fix typo in electricity-meter-schema (previousMonthUnit)

--- src/features/property/schemas/electricity-meter.schema.ts
+++ src/features/property/schemas/electricity-meter.schema.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const ElectricitySchema = z.object({
   meterName: z.string().min(3).max(40),
-  previouseMonthUnit: z.coerce.number().nonnegative(),
+  previousMonthUnit: z.coerce.number().nonnegative(),
   previousUnitDate: z.coerce.date(),
 });
 
